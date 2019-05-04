b3c401db428bcfc2d118d35d3fa453d6488a479c
Muhammad Alfarisyi <echofoxstrot@gmail.com>
2019-05-04 18:49:12 +0700

    penyempurnaan

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://localhost/rpl/INF206-2019-A/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = '';
+$config['index_page'] = ' ';
 
 /*
 |--------------------------------------------------------------------------
