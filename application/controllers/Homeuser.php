dce897973ceb817231b781a6192ca24ac4b50c6f
Dicky Rizki Giatama <dickyrizkigiatama@gmail.com>
2019-05-12 16:09:35 +0700

    fix

--- application/controllers/Homeuser.php
+++ application/controllers/Homeuser.php
@@ -3,11 +3,7 @@ class Homeuser extends CI_controller
 {
     public function index()
     {
-        if (isset($_SESSION['username'])){
-            $data['user'] = $this->db->get_where('user', ['username' => $this->session->userdata('username')])->row_array();
-            $this->load->view('user/homeuser', $data);
-        }else{
-            redirect('login');
-        }
+        $data['user'] = $this->db->get_where('user', ['username' => $this->session->userdata('username')])->row_array();
+        $this->load->view('user/homeuser', $data);
     }
 }
