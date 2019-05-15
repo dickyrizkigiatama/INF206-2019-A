f2b5caf9b76d6f75a7dee86d215c6db9eac116da
MaulanaHz <hizbullahmaulana1@gmail.com>
2019-05-15 13:32:18 +0700

    update

--- application/controllers/Homeuser.php
+++ application/controllers/Homeuser.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 class Homeuser extends CI_controller
 {
@@ -12,19 +11,4 @@ class Homeuser extends CI_controller
             redirect('login');
         }
     }
-=======
-<?php
-class Homeuser extends CI_controller
-{
-    public function index()
-    {
-        if(isset($_SESSION['username'])){
-            $data['user'] = $this->db->get_where('user', ['username' => $this->session->userdata('username')])->row_array();
-            $this->load->view('user/homeuser', $data);
-        }
-        else{
-            redirect('login');
-        }
-    }
->>>>>>> master
 }
