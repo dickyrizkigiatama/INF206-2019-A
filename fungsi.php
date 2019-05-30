d63998e30d0546e373245429e71b168a80fa4bd5
Wirda <wirdayanni@gmail.com>
2019-05-30 12:31:33 +0700

    commit 2

--- fungsi.php
+++ fungsi.php
@@ -24,7 +24,6 @@ if(isset($_POST['login'])){
     header("Location: masukUser.html");
     exit;
 
-
 function daftarAsuransi(){
     global $koneksi;
     $namadepan = htmlspecialchars($data["nama_depan"]);
@@ -32,30 +31,4 @@ function daftarAsuransi(){
     $tempatlahir = htmlspecialchars($data["tempat_lahir"]);
     $tanggallahir = htmlspecialchars($data["tanggal_lahir"]);
 }
-=======
-<?php
-$koneksi=mysqli_connect("localhost","root","","masin");
-if(isset($_POST['login'])){
-    $username=$_POST['username'];
-    $password=$_POST['password'];
-
-    $simpan=mysqli_query($koneksi,"SELECT * FROM user WHERE username='$username' AND password='$password'");
-
-    if(mysqli_num_rows($simpan)==1){
-        $role= mysqli_fetch_assoc($simpan);
-        if($role['role']=="user"){
-            header("Location: List Perusahaan sisi Nelayan.php");
-            exit;
-        }else{
-            header("Location: generic.html");
-            exit;
-        }
-    }else{
-        header("Location: masukUser.html");
-        exit;
-    }
-}else{
-    header("Location: masukUser.html");
-    exit;
->>>>>>> Anggi
-}
+?>
